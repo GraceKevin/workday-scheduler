d8a8114068beb9694c4afcc6eec3d44a07218167
Kevin Grace <Kevin@kevins-air.home>
2021-07-24 19:15:42 -0400

    added font awesome for save buttons

--- assets/script.js
+++ assets/script.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
 
 // On Click Button  
     $(".saveBtn").on("click", function() {
-        var text = $(this).siblings(".description").value();
-        var time = $(this).parent().attribute("id");
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
 
@@ -23,7 +23,7 @@ $(document).ready(function () {
         $(".time-block").each(function () {
 
             // Set Variable for Block of Time
-            var blockTime = parseInt($(this).attribute("id").split("#hour")[1]);
+            var blockTime = parseInt($(this).attr("id").split("#hour")[1]);
 
             //Check Current Time in order to add background classes
             if (blockTime < currentTime) {
@@ -45,16 +45,16 @@ $(document).ready(function () {
 
     // Retrieve from local storage
 
-    $("#8am.description").value(localStorage.getItem("8am"));
-    $("#9am.description").value(localStorage.getItem("9am"));
-    $("#10am.description").value(localStorage.getItem("10am"));
-    $("#11am.description").value(localStorage.getItem("11am"));
-    $("#12pm.description").value(localStorage.getItem("12pm"));
-    $("#1pm.description").value(localStorage.getItem("1pm"));
-    $("#2pm.description").value(localStorage.getItem("2pm"));
-    $("#3pm.description").value(localStorage.getItem("3pm"));
-    $("#4pm.description").value(localStorage.getItem("4pm"));
-    $("#5pm.description").value(localStorage.getItem("5pm"));
+    $("#8am.description").val(localStorage.getItem("8am"));
+    $("#9am.description").val(localStorage.getItem("9am"));
+    $("#10am.description").val(localStorage.getItem("10am"));
+    $("#11am.description").val(localStorage.getItem("11am"));
+    $("#12pm.description").val(localStorage.getItem("12pm"));
+    $("#1pm.description").val(localStorage.getItem("1pm"));
+    $("#2pm.description").val(localStorage.getItem("2pm"));
+    $("#3pm.description").val(localStorage.getItem("3pm"));
+    $("#4pm.description").val(localStorage.getItem("4pm"));
+    $("#5pm.description").val(localStorage.getItem("5pm"));
 
     hourStatus();
 
