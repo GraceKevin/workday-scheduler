54738fb26fc7192434d3937b8444de6e2ca44c80
Kevin Grace <Kevin@kevins-air.home>
2021-07-24 21:03:19 -0400

    updated JS

--- assets/script.js
+++ assets/script.js
@@ -18,23 +18,25 @@ $(document).ready(function () {
     function hourStatus() {
         
         // Current Number of Hours 
-        var currentTime = moment().hour();
+        var currentTime = moment().format('LLLL');
 
         $(".time-block").each(function () {
 
             // Set Variable for Block of Time
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            var blockTime = parseInt($(this).attr("id").split("hour") [1] );
 
             //Check Current Time in order to add background classes
             if (blockTime < currentTime) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
                 $(this).addClass("past");
-            } else if (blockTime === currentTime) {
+            } 
+            else if (blockTime === currentTime) {
                 $(this).removeClass("past");
                 $(this).removeClass("future");
                 $(this).addClass("present");
-            } else {
+            } 
+            else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
@@ -45,19 +47,18 @@ $(document).ready(function () {
 
     // Retrieve from local storage
 
-    $("#8am.description").val(localStorage.getItem("8am"));
-    $("#9am.description").val(localStorage.getItem("9am"));
-    $("#10am.description").val(localStorage.getItem("10am"));
-    $("#11am.description").val(localStorage.getItem("11am"));
-    $("#12pm.description").val(localStorage.getItem("12pm"));
-    $("#1pm.description").val(localStorage.getItem("1pm"));
-    $("#2pm.description").val(localStorage.getItem("2pm"));
-    $("#3pm.description").val(localStorage.getItem("3pm"));
-    $("#4pm.description").val(localStorage.getItem("4pm"));
-    $("#5pm.description").val(localStorage.getItem("5pm"));
+    $("#8am .description").val(localStorage.getItem("8am"));
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
 
     hourStatus();
-
 })
 
 
